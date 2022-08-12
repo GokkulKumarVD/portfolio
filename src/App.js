e21369416f50d19b670cd1b2334b559cd1e6177d
gokkul kumar vd <vd.gokkulkumar@swiggy.in>
2022-08-12 20:51:50 +0530

    3 services section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // npm i react-router-dom framer-motion react-intersection-observer styled-components
 import React from "react";
+import Aboutus from "./pages/Aboutus";
 
 function App() {
   return (
     <div className="App">
-
+      <Aboutus />
     </div>
   );
 }
---
+++ src/pages/Aboutus.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Aboutsection from "../components/Aboutsection";
+import Servicesection from "../components/Servicesection";
+
+const Aboutus = () => {
+  return (
+    <div>
+      <Aboutsection />
+      <Servicesection />
+    </div>
+  );
+};
+
+export default Aboutus;
