58baefcfcddb27791050038450da55e606c540b5
gokkul kumar vd <vd.gokkulkumar@swiggy.in>
2022-08-12 22:22:09 +0530

    4 faq section

---
+++ src/components/Faqsection.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+const Faqsection = () => {
+    return (
+        <div className="faq">
+            <h2>Any question <span>FAQ</span> </h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptates.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, pariatur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Payment method?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptates.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, pariatur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What do you offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptates.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, pariatur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>How to login?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptates.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, pariatur.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>How to make payment?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, voluptates.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, pariatur.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Faqsection
--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Aboutsection from "../components/Aboutsection";
 import Servicesection from "../components/Servicesection";
+import Faqsection from "./components/Faqsection";
+
 
 const Aboutus = () => {
   return (
     <div>
       <Aboutsection />
       <Servicesection />
+      <Faqsection />
     </div>
   );
 };
